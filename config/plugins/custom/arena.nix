c0d27412fc02898f0df6817888abb6c93587d621
Nathaniel Tooley <nathanieltooley24@gmail.com>
2026-09-15 09:24:14 -0500

    finish arena setup

--- config/plugins/custom/arena.nix
+++ config/plugins/custom/arena.nix
@@ -3,11 +3,24 @@
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
       name = "arena";
-      src = pkgs.fetchFromGithub {
+      src = pkgs.fetchFromGitHub {
         owner = "dzfrias";
         repo = "arena.nvim";
         rev = "9ebbf55b4f0e3815e3caf3264cc1a2721b5111e9";
+        hash = "sha256-55jXZdtFWVfzUgfvy3FyglLjIuktVqILzGFGODCSeTY=";
       };
     })
+  ];
+
+  extraConfigLua = ''
+    require('arena').setup()
+  '';
+
+  keymaps = [
+    {
+      key = "<leader>sb";
+      action.__raw = ''function() require("arena").toggle() end'';
+      options.desc = "Toggle Arena buffer viewer";
+    }
   ];
 }
