63d14dfc92073e9e30058a60e840fe8b3ea50b56
Nathaniel Tooley <nathanieltooley24@gmail.com>
2026-09-16 01:02:30 -0500

    add early-retirement plugin

--- config/plugins/custom/early-retirement.nix
+++ config/plugins/custom/early-retirement.nix
@@ -7,11 +7,12 @@
         owner = "chrisgrieser";
         repo = "nvim-early-retirement";
         rev = "8ddd369731c3a123a062ccc75c5c9ed54ffec9c1";
+        hash = "sha256-3GSe2H3wnnG+BD6fsLClMySUwxkMaG1lqX1mHbnmgwI=";
       };
     })
   ];
 
   extraConfigLua = ''
-    require('nvim-early-retirement').setup()
+    require('early-retirement').setup({})
   '';
 }
