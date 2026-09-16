aacb6b2281b7f101621579c5f109c0e10e70736c
Nathaniel Tooley <nathanieltooley24@gmail.com>
2026-09-16 10:49:59 -0500

    enable snippets

--- config/plugins/kickstart/blink-cmp.nix
+++ config/plugins/kickstart/blink-cmp.nix
@@ -3,9 +3,9 @@
   #    See the README about individual language/framework/plugin snippets:
   #    https://github.com/rafamadriz/friendly-snippets
   # https://nix-community.github.io/nixvim/plugins/friendly-snippets.html
-  # plugins.friendly-snippets = {
-  #   enable = true;
-  # };
+  plugins.friendly-snippets = {
+    enable = true;
+  };
 
   # Dependencies
   #
@@ -44,7 +44,6 @@
         #
         # See :h blink-cmp-config-keymap for defining your own keymap
         preset = "default";
-
         # For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         #    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       };
