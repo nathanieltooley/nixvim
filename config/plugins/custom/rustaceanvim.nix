7f50ac178e035a109722aa3843e421eeeedb1fab
Nathaniel Tooley <nathanieltooley24@gmail.com>
2026-09-16 01:30:52 -0500

    configure rustvim

--- config/plugins/custom/rustaceanvim.nix
+++ config/plugins/custom/rustaceanvim.nix
@@ -1,3 +1,14 @@
 {
   plugins.rustaceanvim.enable = true;
+  plugins.rustaceanvim.settings = {
+    server = {
+      default_settings = {
+        rust-analyzer = {
+          check = {
+            command = "clippy";
+          };
+        };
+      };
+    };
+  };
 }
