d14dc26431505235aff60190b36c5e9abf8d2855
Nathaniel Tooley <nathanieltooley24@gmail.com>
2026-09-17 00:40:10 -0500

    add terminal toggle keybind

--- nixvim.nix
+++ nixvim.nix
@@ -149,6 +149,14 @@ in
       key = "-";
       action.__raw = "function() _G.MiniFiles.open() end";
     }
+    {
+      mode = "n";
+      key = "<leader>tt";
+      action = "<cmd>ToggleTerm size=15 direction=horizontal<CR>";
+      options = {
+        desc = "[T]oggle [T]erminal";
+      };
+    }
     # Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
     # for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
     # is not what someone will guess without a bit more experience.
